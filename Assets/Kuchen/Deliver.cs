430f2433e569a419f039845e7bb43d8dbe422fad
kyubuns <kyubuns@gmail.com>
2016-02-15 21:03:45 +0900

    Publish/Subscribe実装完成

--- Assets/Kuchen/Deliver.cs
+++ Assets/Kuchen/Deliver.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace Kuchen
 {
 	public class Deliver
@@ -12,14 +15,38 @@ namespace Kuchen
 			}
 		}
 		
+		private List<Subscriber> subscribers = new List<Subscriber>();
+		private List<Action> bookedEvents;
+		
+		public int Publish(string topic, object[] args)
+		{
+			int listener = 0;
+			bool rootEvent = false;
+			if(bookedEvents == null)
+			{
+				rootEvent = true;
+				bookedEvents = new List<Action>();
+			}
+			foreach(var subscriber in subscribers) listener += subscriber.Call(topic, args);
+			if(rootEvent)
+			{
+				var copied = bookedEvents.ToArray();
+				bookedEvents = null;
+				foreach(var e in copied) e();
+			}
+			return listener;
+		}
+		
 		public void Subscribe(Subscriber subscriber)
 		{
-			// TODO
+			if(bookedEvents != null) bookedEvents.Add(() => { Subscribe(subscriber); });
+			else subscribers.Add(subscriber);
 		}
 		
 		public void Unsubscribe(Subscriber subscriber)
 		{
-			// TODO
+			if(bookedEvents != null) bookedEvents.Add(() => { Unsubscribe(subscriber); });
+			else subscribers.Remove(subscriber);
 		}
 	}
 }
--- Assets/Kuchen/Publisher.cs
+++ Assets/Kuchen/Publisher.cs
@@ -2,5 +2,19 @@
 {
 	public class Publisher
 	{
+		public static int Publish(string topic)
+		{
+			return Deliver.Instance.Publish(topic, new object[]{});
+		}
+		
+		public static int Publish<T1>(string topic, T1 arg1)
+		{
+			return Deliver.Instance.Publish(topic, new object[]{arg1});
+		}
+		
+		public static int Publish<T1, T2>(string topic, T1 arg1, T2 arg2)
+		{
+			return Deliver.Instance.Publish(topic, new object[]{arg1, arg2});
+		}
 	}
 }
