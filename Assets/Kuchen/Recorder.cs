1d31ef443d0fe9be772aef150c43403928d1e7be
kyubuns <kyubuns@gmail.com>
2016-02-16 14:56:35 +0900

    Recorder

---
+++ Assets/Kuchen/Recorder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kuchen
+{
+	public class Recorder : IDisposable
+	{
+		private Subscriber subscriber;
+		public List<string> Topics { get; private set; }
+
+		public Recorder()
+		{
+			subscriber = new Subscriber();
+			Topics = new List<string>();
+			subscriber.Subscribe("*", (topic) => { Topics.Add(topic); });
+		}
+
+		public void Dispose()
+		{
+			subscriber.Dispose();
+		}
+
+		public bool Contains(string topic) { return Topics.Contains(topic); }
+	}
+}
