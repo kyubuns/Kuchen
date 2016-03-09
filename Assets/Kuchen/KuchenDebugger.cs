fce4296d91303f908e7e12c23457a30b688ecb0d
kyubuns <kyubuns@gmail.com>
2016-03-10 01:36:29 +0900

    デバッガー系スクリプト強化

--- Assets/Kuchen/KuchenDebugger.cs
+++ Assets/Kuchen/KuchenDebugger.cs
@@ -4,6 +4,10 @@ namespace Kuchen
 {
 	public class KuchenDebugger : MonoBehaviour
 	{
+		public string Topic;
+		public string Arg1;
+		public bool SendTopic;
+		
 		public void Start()
 		{
 			this.SubscribeWithTopic("*", (string topic, object arg1, object arg2, object arg3) => {
@@ -12,6 +16,18 @@ namespace Kuchen
 				else if(arg3 == null) Debug.LogFormat("[KuchenDebugger] Topic:{0} | Arg1:{1} Arg2:{2}", topic, arg1, arg2);
 				else Debug.LogFormat("[KuchenDebugger] Topic:{0} | Arg1:{1} Arg2:{2} Arg3:{3}", topic, arg1, arg2, arg3);
 			});
+		}
+		
+		public void Update()
+		{
+			if(SendTopic)
+			{
+				if(string.IsNullOrEmpty(Arg1)) Kuchen.Publisher.Publish(Topic);
+				else Kuchen.Publisher.Publish(Topic, Arg1);
+				SendTopic = false;
+				Topic = "";
+				Arg1 = "";
+			}
 		}
 		
 		public void OnEnable()
--- Assets/Kuchen/Recorder.cs
+++ Assets/Kuchen/Recorder.cs
@@ -1,23 +1,25 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+using UniRx;
 
 namespace Kuchen
 {
 	public class Recorder : IDisposable
 	{
 		private Subscriber subscriber;
-		public List<string> Topics { get; private set; }
+		public List<Tuple<string, object, object, object>> Topics { get; private set; }
 
 		public Recorder()
 		{
 			subscriber = new Subscriber();
-			Topics = new List<string>();
-			subscriber.SubscribeWithTopic("*", Record);
+			Topics = new List<Tuple<string, object, object, object>>();
+			subscriber.SubscribeWithTopic<object, object, object>("*", Record);
 		}
 		
-		private void Record(string topic)
+		private void Record(string topic, object arg1, object arg2, object arg3)
 		{
-			Topics.Add(topic);
+			Topics.Add(new Tuple<string, object, object, object>(topic, arg1, arg2, arg3));
 		}
 
 		public void Dispose()
@@ -25,6 +27,22 @@ namespace Kuchen
 			subscriber.Dispose();
 		}
 
-		public bool Contains(string topic) { return Topics.Contains(topic); }
+		public bool Contains(string topic) { return Topics.Any(x => x.Item1 == topic); }
+		public Tuple<string, object, object, object> Find(string topic) { return Topics.Find(x => x.Item1 == topic); }
+		public Tuple<string, T1> Find<T1>(string topic)
+		{
+			var t = Find(topic);
+			return new Tuple<string, T1>(t.Item1, (T1)t.Item2);
+		}
+		public Tuple<string, T1, T2> Find<T1, T2>(string topic)
+		{
+			var t = Find(topic);
+			return new Tuple<string, T1, T2>(t.Item1, (T1)t.Item2, (T2)t.Item3);
+		}
+		public Tuple<string, T1, T2, T3> Find<T1, T2, T3>(string topic)
+		{
+			var t = Find(topic);
+			return new Tuple<string, T1, T2, T3>(t.Item1, (T1)t.Item2, (T2)t.Item3, (T3)t.Item4);
+		}
 	}
 }
